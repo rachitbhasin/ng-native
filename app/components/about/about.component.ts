8bd6d731cfe173adda697815e9045378351b7bf4
Rachit Bhasin <RB@blinfo.se>
2017-01-27 19:49:36 +0530

    Added modal popup

--- app/components/about/about.component.ts
+++ app/components/about/about.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/directives/dialogs'
+import {DetailComponent} from '../detail/detail.component';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +8,15 @@ import {Component} from '@angular/core';
     templateUrl: 'about.component.html'
 })
 export class AboutComponent {
+    constructor(private modal: ModalDialogService, private viewRef: ViewContainerRef){}
 
+    public openModal(){
+        let options: ModalDialogOptions = {
+            context: { msg: 'Hello' },
+            viewContainerRef: this.viewRef
+        }
+        this.modal.showModal(DetailComponent, options).then(result => {
+            console.log(result);
+        });
+    }
 }
--- app/components/detail/detail.component.ts
+++ app/components/detail/detail.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { ModalDialogParams } from 'nativescript-angular/directives/dialogs';
 
 @Component({
     moduleId: module.id,
     selector: 'detail',
-    templateUrl: 'detail.component.html'
+    templateUrl: 'detail.component.html',
+    styles: [`
+        .odd{background-color: paleturquoise;width: 100%}
+        .even{background-color: aliceblue;width: 100%}
+        
+     `]
 })
 export class DetailComponent {
 
-    constructor(private router: RouterExtensions) { }
+    public items: any[] = [
+        { title: 'NativeScript' },
+        { title: 'Angular' },
+        { title: 'TypeScript' },
+        { title: 'JavaScript' }
+    ];
+
+    constructor(private router: RouterExtensions, private params: ModalDialogParams) {
+        console.log(params.context.msg);
+    }
+
+    public onItemTap(e) {
+        for (let key in e) {
+            console.log(`${key}: ${e[key]}`);
+        }
+    }
 
     public goBack() {
         this.router.back();
+    }
+
+    public close() {
+        this.params.closeCallback('closed the ModalDialogOptions.');
     }
 }
